39c32d38cf6e708a2d0f9c8c961ab0c2e9e2295f
Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>
2026-09-08 09:34:14 +0200

    added MAVEN_CENTRAL_USERNAME/CENTRAL_PASSWORD

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ publishing {
         mavenLocal()
         maven {
             name = "sonatype"
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val releasesRepoUrl = "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
             val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
             url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
             credentials {
